ff7487b9b6e48ad414852a3f909ac903d09dc729
Nuno Street <nuno.dslemos@gmail.com>
2026-02-19 10:28:30 +0000

    Generator skeleton

--- maze/generator.py
+++ maze/generator.py
@@ -1,12 +1,27 @@
+from .cell import ALL_WALLS, DIRECTIONS
+
+
 class MazeGenerator:
     def __init__(self, config):
-        ...
+        self.width = config.width
+        self.height = config.height
+        self.entry = config.entry
+        self.exit = config.exit
+        self.perfect = config.perfect
+        self.seed = config.seed
+        
+        self.grid = []
+        self.solution = []
 
     def generate(self) -> None:
-        ...
+        self._init_grid()
 
     def get_grid(self) -> list[list[int]]:
-        ...
+        self.grid = [
+            [ALL_WALLS for _ in range (self.width)]
+            for _ in range(self.height)
+        ]
+        return self.grid
 
     def get_solution(self) -> list[str]:
         ...
